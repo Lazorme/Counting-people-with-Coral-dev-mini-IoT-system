38909f5d52be76d7e823569158c81b5203f4ba98
Baptiste <114988681+Lazorme@users.noreply.github.com>
2023-07-12 14:49:35 +0200

    Add files via upload

--- detect2.py
+++ detect2.py
@@ -68,68 +68,71 @@ def main():
     mot_tracker = Sort(max_age=3,min_hits=4,iou_threshold=0.4) #create instance of the SORT tracker
 
     while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            print('No data , please check the camera!')
-            break
-        fps = cap.get(cv2.CAP_PROP_FPS)
-        cv2_im = frame
+        if button.read()==1:
+            ret, frame = cap.read()
+            if not ret:
+                print('No data , please check the camera!')
+                break
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            cv2_im = frame
 
-        #Check if the motion sensor detect people 
-        #if not button.read():
-            #break
+         
 
-        #Display the people counter
-        cv2.putText(cv2_im, f"Personnes enters: {person_counter_enter}", (10, 70), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
-        cv2.putText(cv2_im, f"Personnes leaves: {person_counter_leave}", (10, 50), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+            #Display the people counter
+            cv2.putText(cv2_im, f"Personnes enters: {person_counter_enter}", (10, 70), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+            cv2.putText(cv2_im, f"Personnes leaves: {person_counter_leave}", (10, 50), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
 
-        #Resize corectly and run interference
-        if cv2_im.shape[:2] != inference_size:
-            cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
-            cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
-            run_inference(interpreter, cv2_im_rgb.tobytes())
-        else:
-            run_inference(interpreter, cv2_im.tobytes())
+            #Resize corectly and run interference
+            if cv2_im.shape[:2] != inference_size:
+                cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
+                cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
+                run_inference(interpreter, cv2_im_rgb.tobytes())
+            else:
+                run_inference(interpreter, cv2_im.tobytes())
 
-        #Create list of objects
-        objs = get_objects(interpreter, args.threshold)[:args.top_k]
+            #Create list of objects
+            objs = get_objects(interpreter, args.threshold)[:args.top_k]
     
-        #Filtering
-        objs = [obj for obj in objs if obj.score >= 0.3 and labels.get(obj.id, obj.id) == "person"]  # Filter objects with score >= 0.3 and label == "person"
-        objs.sort(key=lambda obj: obj.score, reverse=True)  # Sort objects by score in descending order
+            #Filtering
+            objs = [obj for obj in objs if obj.score >= 0.3 and labels.get(obj.id, obj.id) == "person"]  # Filter objects with score >= 0.3 and label == "person"
+            objs.sort(key=lambda obj: obj.score, reverse=True)  # Sort objects by score in descending order
 
 
-        #Initialise the sort tracker
-        detections = np.array([[obj.bbox.xmin, obj.bbox.ymin, obj.bbox.xmax, obj.bbox.ymax, obj.score] for obj in objs])
-        trdata = []
-        trackerFlag = False
+            #Initialise the sort tracker
+            detections = np.array([[obj.bbox.xmin, obj.bbox.ymin, obj.bbox.xmax, obj.bbox.ymax, obj.score] for obj in objs])
+            trdata = []
+            trackerFlag = False
 
-        #Extract dimension of input image
-        height, width, _ = cv2_im.shape
+            #Extract dimension of input image
+            height, width, _ = cv2_im.shape
 
-        # Draw detection lines
-        cv2.line(cv2_im, (line1_x, 0), (line1_x, height), (0, 255, 255), 1)  # Yellow line for the first detection line
-        cv2.line(cv2_im, (line2_x, 0), (line2_x, height), (255, 255, 0), 1)  # Cyan line for the second detection line
+            # Draw detection lines
+            cv2.line(cv2_im, (line1_x, 0), (line1_x, height), (0, 255, 255), 1)  # Yellow line for the first detection line
+            cv2.line(cv2_im, (line2_x, 0), (line2_x, height), (255, 255, 0), 1)  # Cyan line for the second detection line
 
-        #Update tracker and display detection
-        if detections.any():
-            if mot_tracker != None:
-                trdata = mot_tracker.update(detections)
-                trackerFlag = True
-        if len(objs) != 0:
-            cv2_im,person_counter_enter,person_counter_leave = append_objs_to_img(cv2_im, inference_size, objs, labels,trdata,trackerFlag,line1_x,line2_x,entered_left_ids,exited_left_ids,height,width,person_counter_enter,entered_right_ids,exited_right_ids,person_counter_leave)
+            #Update tracker and display detection
+            if detections.any():
+                if mot_tracker != None:
+                    trdata = mot_tracker.update(detections)
+                    trackerFlag = True
+            if len(objs) != 0:
+                cv2_im,person_counter_enter,person_counter_leave = append_objs_to_img(cv2_im, inference_size, objs, labels,trdata,trackerFlag,line1_x,line2_x,entered_left_ids,exited_left_ids,height,width,person_counter_enter,entered_right_ids,exited_right_ids,person_counter_leave)
 
-        #Show FPS
-        #frame_count += 1
-        #if frame_count % fps_update_interval == 0:
-            #elapsed_time = time.time() - start_time
-            #fps = frame_count / elapsed_time           
-        cv2.putText(cv2_im, f"FPS: {round(fps, 2)}", text_position, font, font_scale, font_color, font_thickness)
-        cv2.imshow('frame', cv2_im)
+            #Show FPS
+            #frame_count += 1
+            #if frame_count % fps_update_interval == 0:
+                #elapsed_time = time.time() - start_time
+                #fps = frame_count / elapsed_time           
+            cv2.putText(cv2_im, f"FPS: {round(fps, 2)}", text_position, font, font_scale, font_color, font_thickness)
+            cv2.imshow('frame', cv2_im)
         
-        #Stop the detection
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            #Stop the detection
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            
+        else:
+            print('waiting')
+            time.sleep(1)
 
     button.close()
     cap.release()
@@ -204,10 +207,4 @@ def append_objs_to_img(cv2_im, inference_size, objs, labels,trdata,trackerFlag,l
 if __name__ == '__main__':
     #Define motion sensor
     button = GPIO("/dev/gpiochip0", 13, "in")  # pin 36
-    while True:
-      if button.read()==1:
-        main()
-        button.close()
-      else:
-        print("Waiting")
-      time.sleep(1)
+    main()
