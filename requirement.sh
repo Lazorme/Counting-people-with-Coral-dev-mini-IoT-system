6f1d024caf01cd32777a5c45c6d528ae0ba6fc2d
Baptiste <114988681+Lazorme@users.noreply.github.com>
2023-07-19 14:17:24 +0200

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -2,9 +2,10 @@ echo
 echo "Installing all library."
 echo
 
-sudo apt install opencv_python
-sudo apt install python3-skimage
-sudo apt install python3-dev
+sudo apt-get install -y python3-pip python3-edgetpuvision
+sudo pip3 install opencv_python
+sudo pip3 install python3-skimage
+sudo pip3 install python3-dev
 python3 -m pip install -r requirements_tracker.txt
 
 echo
