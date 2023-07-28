c2efe831bb8025817850bdbbeeeb4e63c9a70c70
Baptiste <114988681+Lazorme@users.noreply.github.com>
2023-07-28 14:53:39 +0200

    Update detect.py

--- detect.py
+++ detect.py
@@ -257,6 +257,6 @@ if __name__ == '__main__':
     subprocess.run(command, shell=True)
 
     #Define motion sensor
-    button = GPIO("/dev/gpiochip0", 13, "in")  # pin 36
+    button = GPIO("/dev/gpiochip0", 38, "in")  # pin 38
     port = serial.Serial("/dev/ttyS1", baudrate=9600, timeout =1)
     main()
